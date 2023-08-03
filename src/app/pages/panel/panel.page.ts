b69705993c7bf07d21acc25c476cb5ab3b9d6259
Raúl Mariscal <ra.mariscal@duocuc.cl>
2023-08-03 03:17:23 -0400

    Arreglo de xs

--- src/app/pages/panel/panel.page.ts
+++ src/app/pages/panel/panel.page.ts
@@ -7,9 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanelPage implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {  }
+  
 }
