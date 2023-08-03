da73a3eecdcfa1047227ea89ce250d4f8cfbf5b7
Raúl Mariscal <ra.mariscal@duocuc.cl>
2023-08-03 03:38:42 -0400

    creación de footer

--- src/app/componentes/componentes.module.ts
+++ src/app/componentes/componentes.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogoComponent } from './logo/logo.component';
 import { IonicModule } from '@ionic/angular';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    LogoComponent
+    LogoComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     IonicModule
   ],
   exports: [
-    LogoComponent
+    LogoComponent,
+    FooterComponent
   ]
 })
 export class ComponentesModule { }
